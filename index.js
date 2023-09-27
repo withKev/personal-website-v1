876d40a12cd602bc439e1c1c095b7867f9c0aea6
Kevin Truong <contact@kevintruong.ca>
2023-09-27 14:46:33 -0700

    Add smoothscroll and  reveal sections with nav

---
+++ index.js
@@ -0,0 +1,28 @@
+// Smooth scroll to anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", revealSections);
+
+function revealSections() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (sectionTop < windowHeight * 0.7) {
+      section.classList.add("active");
+    }
+  });
+}
+
+// Initially reveal sections that are in the viewport on page load
+revealSections();
