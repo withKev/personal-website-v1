e27421a43900b63380a16576908c6ea92d3e4640
Kevin Truong <contact@kevintruong.ca>
2023-09-27 21:35:00 -0700

    Adds floating button to navigate page

--- index.js
+++ index.js
@@ -26,3 +26,37 @@ function revealSections() {
 
 // Initially reveal sections that are in the viewport on page load
 revealSections();
+
+const floatingButton = document.querySelector(".fb");
+let currentSectionIndex = 0;
+
+floatingButton.addEventListener("click", function () {
+  const nextSectionIndex = currentSectionIndex + 1;
+  if (nextSectionIndex === sections.length - 1) {
+    floatingButton.classList.remove("fa-caret-down");
+    floatingButton.classList.add("fa-caret-up");
+  } else {
+    floatingButton.classList.remove("fa-caret-up");
+    floatingButton.classList.add("fa-caret-down");
+  }
+
+  if (nextSectionIndex < sections.length) {
+    const nextSection = sections[nextSectionIndex];
+    const nextSectionTop =
+      nextSection.getBoundingClientRect().top + window.scrollY;
+
+    window.scrollTo({
+      top: nextSectionTop,
+      behavior: "smooth",
+    });
+
+    currentSectionIndex = nextSectionIndex;
+  } else {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    currentSectionIndex = 0;
+  }
+});
